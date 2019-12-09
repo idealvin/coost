31d42c879b6414bd8d5ff2c2b73e20205d5ec112
Alvin <idealvin@qq.com>
2019-12-09 14:02:36 +0800

    update xmake.lua

--- rpcgen/xmake.lua
+++ rpcgen/xmake.lua
@@ -1,18 +1,20 @@
-target("base")
-    set_kind("static")
+target("rpcgen")
+    set_kind("binary")
     add_cxxflags("-std=c++11")
     set_optimize("faster")  -- -O2
     set_warnings("all")     -- -Wall
-    set_targetdir("../lib")
+    set_targetdir("../build")
     add_includedirs("..")
-    add_files("**.cc")
+    add_linkdirs("../lib")
+    add_links("base")
+    add_files("*.cc")
 
     if is_plat("macosx", "linux") then
         add_cxflags("-g3")
         if is_plat("macosx") then
             add_cxflags("-fno-pie")
         end
-        add_files("co/context/context.S")
+        add_links("pthread", "dl")
         after_build(function ()
             os.run("rm -rf build")
         end)
@@ -20,14 +22,7 @@ target("base")
 
     if is_plat("windows") then
         add_cxflags("-Ox", "-fp:fast", "-EHsc")
-        add_files("**.cpp")
-        if is_arch("x64") then
-            add_files("co/context/context_x64.asm")
-        else
-            add_files("co/context/context_x86.asm")
-        end
         after_build(function ()
             os.run("cmd /k rd /s /q build; exit")
         end)
     end
-
--- unitest/base/xmake.lua
+++ unitest/base/xmake.lua
@@ -1,19 +1,28 @@
 target("unitest")
     set_kind("binary")
     add_cxxflags("-std=c++11")
-    set_optimize("faster")
+    set_optimize("faster")  -- -O2
+    set_warnings("all")     -- -Wall
     set_targetdir("../../build")
     add_includedirs("../..")
     add_linkdirs("../../lib")
     add_links("base")
     add_files("*.cc")
+
     if is_plat("macosx", "linux") then
-        add_cxflags("-g3", "-Wall")
-        add_ldflags("-lpthread -ldl")
+        add_cxflags("-g3")
+        if is_plat("macosx") then
+            add_cxflags("-fno-pie")
+        end
+        add_links("pthread", "dl")
+        after_build(function ()
+            os.run("rm -rf build")
+        end)
     end
-    if is_plat("macosx") then
-        add_cxflags("-fno-pie")
-    end
+
     if is_plat("windows") then
-        add_cxflags("-Ox", "-fp:fast")
+        add_cxflags("-Ox", "-fp:fast", "-EHsc")
+        after_build(function ()
+            os.run("cmd /k rd /s /q build; exit")
+        end)
     end
