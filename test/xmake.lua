e7d0a151f3197f5c0834f19aaa50e10ddc2c7112
Alvin <idealvin@qq.com>
2020-01-09 10:07:17 +0800

    improve test/xmake.lua

--- test/xmake.lua
+++ test/xmake.lua
@@ -1,8 +1,14 @@
+function all_tests()
+    local i = 1
+    local res = {}
+    for _, x in ipairs(os.files("*_test.cc")) do
+        res[i] = x:sub(1, #x - 8)
+        i = i + 1
+    end
+    return res
+end
 
-for _, test in ipairs({
-    "co", "fast", "flag", "hash", "json", "rapidjson", "log",
-    "rpc", "stack", "str", "task_sched", "time", "tw", "xx"
-}) do
+for _, test in ipairs(all_tests()) do
 target(test)
     set_kind("binary")
     set_default(false)
@@ -10,4 +16,3 @@ target(test)
 --  add_links("jemalloc")
     add_files(test .. "_test.cc")
 end
-
