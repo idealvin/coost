f9aa9c39d10c73364dbfe1489f2403132a471667
Alvin <idealvin@qq.com>
2020-01-14 16:44:07 +0800

    add base/base.h for lazy users to include all header files in co/base

---
+++ base/base.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "def.h"
+#include "atomic.h"
+#include "byte_order.h"
+#include "closure.h"
+
+#include "fast.h"
+#include "fastring.h"
+#include "fastream.h"
+#include "str.h"
+
+#include "time.h"
+#include "thread.h"
+#include "flag.h"
+#include "log.h"
+#include "unitest.h"
+
+#include "random.h"
+#include "lru_map.h"
+#include "hash.h"
+#include "path.h"
+
+#include "fs.h"
+#include "os.h"
+#include "co.h"
+#include "json.h"
+#include "rpc.h"
+
+#include <time.h>
+#include <algorithm>
--- test/xx_test.cc
+++ test/xx_test.cc
@@ -1,26 +1,11 @@
-#include <string>
-#include <map>
-#include <vector>
-#include <unordered_map>
-#include <unordered_set>
-#include <algorithm>
 #include "test.h"
-#include "base/def.h"
-#include "base/log.h"
-#include "base/str.h"
-#include "base/fastream.h"
-#include "base/thread.h"
-#include "base/time.h"
-#include "base/co.h"
-#include "base/json.h"
+#include "base/base.h"
 
 int main(int argc, char** argv) {
     flag::init(argc, argv);
     log::init();
 
     COUT << "hello world";
-
-    // do something here?
 
     return 0;
 }
