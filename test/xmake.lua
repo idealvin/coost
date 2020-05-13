f2f41bbfc8e7cff634359e0e64bc8e002a346541
Alvin <idealvin@qq.com>
2020-05-13 13:20:55 +0800

    improve xmake.lua for co/test

--- test/xmake.lua
+++ test/xmake.lua
@@ -1,17 +1,19 @@
 function all_tests()
     local res = {}
-    for _, x in ipairs(os.files("*_test.cc")) do
-        x = path.filename(x)
-        table.insert(res, x:sub(1, #x - 8))
+    for _, x in ipairs(os.files("**_test.cc")) do
+        local item = {}
+        local s = path.filename(x)
+        table.insert(item, s:sub(1, #s - 8))       -- target
+        table.insert(item, path.relative(x, "."))  -- source
+        table.insert(res, item)
     end
     return res
 end
 
 for _, test in ipairs(all_tests()) do
-target(test)
+target(test[1])
     set_kind("binary")
     set_default(false)
     add_deps("base")
---  add_links("jemalloc")
-    add_files(test .. "_test.cc")
+    add_files(test[2])
 end
