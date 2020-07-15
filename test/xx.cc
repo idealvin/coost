7d2ba782fd7a0be53d498b6fb8cfab8d1b49a324
idealvin <idealvin@163.com>
2020-07-15 20:05:13 +0800

    cleanup test/xx.cc

--- test/xx.cc
+++ test/xx.cc
@@ -1,37 +1,11 @@
 #include "test.h"
 #include "co/all.h"
 
-#include "co/co.h"
-#include "co/log.h"
-#include "co/time.h"
-
-co::Event ev;
-
-DEF_bool(wait_alway, true, "false: wait for timeout, true wait alway");
-void f2(){
-    CLOG << "f2() start";
-    bool r = true;
-    if (FLG_wait_alway) {
-        ev.wait();
-    } else {
-        r = ev.wait(1*1000);
-    }
-    CLOG << "f2() end: " << r;
-}
-
-void f3() {
-    CLOG << "f3() start";
-    ev.signal();
-    CLOG << "f3() end";
-}
-
 int main(int argc, char** argv) {
     flag::init(argc, argv);
     log::init();
-    go(f2);
-    sleep::ms(100);
-    go(f3);
 
-    sleep::ms(2*1000);
+    COUT << "hello world";
+
     return 0;
 }
