5c5724c74dd94e4f41d1c1adcf3af2fb45470d18
idealvin <idealvin@qq.com>
2021-03-17 01:07:38 +0800

    up

--- include/co/jsonv2.h
+++ include/co/jsonv2.h
@@ -1,4 +1,4 @@
-#if 0
+#if 1
 #include "fastream.h"
 #include <vector>
 
@@ -12,22 +12,22 @@ class Mem {
     // @n: initial bytes to be allocated, will be made 8-byte aligned
     explicit Mem(size_t n) {
         const uint32 cap = (uint32)((n >> 3) + !!(n & 7));
-        _mem = (_Mem*) malloc(sizeof(_Mem) + 8 * cap);
-        _mem->cap = cap;
-        _mem->size = 0;
+        _h = (_Header*) malloc(sizeof(_Header) + 8 * cap);
+        _h->cap = cap;
+        _h->size = 0;
     }
 
     ~Mem() {
-        free(_mem);
+        free(_h);
     }
 
     // @n: number of 8-byte to be allocated
     uint32 alloc(uint32 n) {
-        uint32 index = _mem->size;
-        if ((_mem->size += n) > _mem->cap) {
-            _mem->cap += (_mem->cap >> 1);
-            _mem = (_Mem*) realloc(_mem, sizeof(_Mem) + 8 * _mem->cap);
-            assert(_mem);
+        uint32 index = _h->size;
+        if ((_h->size += n) > _h->cap) {
+            _h->cap += (_h->cap >> 1);
+            _h = (_Header*) realloc(_h, sizeof(_Header) + 8 * _h->cap);
+            assert(_h);
         }
         return index;
     }
@@ -54,21 +54,21 @@ class Mem {
     }
 
     void* at(uint32 index) {
-        return _mem->p + index;
+        return _h->p + index;
     }
 
     void clear() {
-        _mem->size = 0;
+        _h->size = 0;
     }
 
   private:
-    struct _Mem {
+    struct _Header {
         uint32 cap;
         uint32 size;
         uint64 p[];
     }; // 8 bytes
 
-    _Mem* _mem;
+    _Header* _h;
 };
 
 class Jalloc {
@@ -99,61 +99,6 @@ inline Jalloc* jalloc() {
     return alloc ? alloc : (alloc = new Jalloc);
 }
 
-// 
-class Array {
-  public:
-    typedef void* T;
-    Array() : _p(0) {}
-
-    explicit Array(uint32 cap) {
-        _p = (T*) xx::jalloc()->alloc(sizeof(T) * cap);
-        _l[-1] = 0;
-    }
-
-    ~Array() {
-        if (_p) xx::jalloc()->dealloc(_p);
-    }
-
-    uint32 size() const {
-        return _l ? _l[-1]: 0;
-    }
-
-    bool empty() const {
-        return this->size() == 0;
-    }
-
-    T& front() const {
-        return _p[0];
-    }
-
-    T& back() const {
-        return _p[_l[-1] - 1];
-    }
-
-    T& operator[](uint32 i) const {
-        return _p[i];
-    }
-
-    void push_back(T v) {
-        if ((_l[-1] + 1) * sizeof(T) >= _l[-2]) {
-            _p = (T*) xx::jalloc()->realloc(_p, _l[-2] << 1);
-            assert(_p);
-        }
-        _p[_l[-1]++] = v;
-    }
-
-    T pop_back() {
-        return _p[--_l[-1]];
-    }
-
-  private:
-    struct _Mem {
-        uint32 size;
-        uint32 next;
-        uint32 p[];
-    };
-    _Mem* _mem;
-};
 } // xx
 
 class Root;
@@ -169,6 +114,21 @@ class Value {
 };
 
 
+// bool: 
+//   | type: 4 | value: 4 |
+//
+// int, double:
+//   | type: 4 | 4 | value: 8 |
+//
+// string:
+//   | type: 4 | size: 4 | body |
+//
+// array, object: 
+//   | type: 4 | size: 4 | cap: 4 | next_index: 4 | body |
+// array, object may have more than one pieces, @next_index points to the next piece.
+//
+// extra pieces for array, object:
+//   | size: 4 | next_index: 4 | body |
 class Root {
   public:
     enum Type {
@@ -187,46 +147,62 @@ class Root {
 
     Root() : _mem(0) {}
 
-    Root(JArray) : _mem(xx::jalloc()->alloc_mem()) {
-        const uint32 index = _mem->alloc_header();
-        assert(index == 0);
-        _Val* v = new (_mem->at(0)) _Val(kArray);
-        v->p = 0;
-    }
-
-    Root(JObject) : _mem(xx::jalloc()->alloc_mem()) {
-        const uint32 index = _mem->alloc(2);
-        assert(index == 0);
-        _Val* v = new (_mem->at(0)) _Val(kObject);
-        v->p = 0;
-    }
-
     ~Root() {
         if (_mem) xx::jalloc()->dealloc_mem(_mem);
     }
 
-    void add_member(Key key, bool x) {
-        uint32 index = _mem->alloc_header();
-        _Val* v = new (_mem->at(index)) _Val(kBool);
-        v->b = x;
+    Root(JArray, uint32 cap=8) : _mem(xx::jalloc()->alloc_mem()) {
+        const uint32 index = _mem->alloc(_b8(16 + sizeof(uint32) * cap));
+        assert(index == 0);
+        _Header* h = _header();
+        h->type = kArray;
+        h->size = 0;
+        h->a[0] = cap;
+        h->a[1] = 0; // next index
+    }
 
+    Root(JObject, uint32 cap=8) : _mem(xx::jalloc()->alloc_mem()) {
+        const uint32 index = _mem->alloc(_b8(16 + 2 * sizeof(uint32) * cap));
+        assert(index == 0);
+        _Header* h = _header();
+        h->type = kObject;
+        h->size = 0;
+        h->a[0] = cap;
+        h->a[1] = 0; // next index
+    }
+
+    void add_member(Key key, bool x) {
+        if (_mem == 0) new (this) Root(JObject());
+        _Header* h = _header();
+        assert(h->type == kObject);
+        
+    }
+
+    void add_array(Key key, uint32 cap=8) {
     }
 
   private:
-    struct _Val {
-        _Val(Type t) : type(t) {}
+    uint32 _b8(uint32 n) {
+        return (uint32)((n >> 3) + !!(n & 7));
+    }
+  private:
+    struct _Header {
+        _Header(Type t) : type(t) {}
         uint32 type;
-        uint32 index;
         union {
-            bool b;   // for bool
-            int64 i;  // for int
-            double d; // for double
-            uint32 p; // for string, array, object
+            bool b;
+            uint32 size;
         };
+        union {
+            int64 i;
+            double d;
+            uint32 a[2]; // for array, object
+        };
+        uint32 p[];
     }; // 16 bytes
 
-    _Val* _val() {
-        return (_Val*)_mem->at(0);
+    _Header* _header() {
+        return (_Header*)_mem->at(0);
     }
 
     xx::Mem* _mem;
