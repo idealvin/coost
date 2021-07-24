812842713a13a150ef95130fa595a420bdcdf20f
idealvin <idealvin@qq.com>
2021-07-24 15:25:00 +0800

    up test/defer.cc

--- test/defer.cc
+++ test/defer.cc
@@ -1,13 +1,26 @@
 #include "co/defer.h"
 #include "co/log.h"
+#include "co/time.h"
 
 void f(int x, int y) {
-    COUT << (x + y);
+    LOG << (x + y);
+}
+
+void f() {
+    Timer t;
+    defer(LOG << "time elapse: " << t.us() << " us");
+    LOG << "hello f()";
 }
 
 int main(int argc, char** argv) {
-    defer(COUT << "hello world");
-    defer(COUT << "hello again");
+    flag::init(argc, argv);
+    log::init();
+    FLG_cout = true;
+
+    defer(LOG << "hello world");
+    defer(LOG << "hello again");
     defer(f(1, 1); f(1, 3));
+    f();
+    f();
     return 0;
 }
