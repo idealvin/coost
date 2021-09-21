1409a28c5d89f308f738de40bbe6070a17da1bdb
idealvin <idealvin@qq.com>
2021-09-22 02:00:59 +0800

    limit co_sched_num to 4 in unitest

--- unitest/main.cc
+++ unitest/main.cc
@@ -1,7 +1,10 @@
 #include "co/unitest.h"
 #include "co/co.h"
 
+DEC_uint32(co_sched_num);
+
 int main(int argc, char** argv) {
+    FLG_co_sched_num = 4;
     co::init(argc, argv);
     unitest::run_all_tests();
     co::exit();
