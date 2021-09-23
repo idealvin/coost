302f46d7e1c946776a68310f039825608821738f
idealvin <idealvin@qq.com>
2021-09-23 20:07:49 +0800

    add unitest in ci

--- unitest/main.cc
+++ unitest/main.cc
@@ -2,12 +2,17 @@
 #include "co/co.h"
 
 DEC_uint32(co_sched_num);
+DEC_bool(noco);
 
 int main(int argc, char** argv) {
     flag::init(argc, argv);
-    FLG_co_sched_num = 8;
-    co::init();
+    if (!FLG_noco) {
+        if (FLG_co_sched_num > 8) FLG_co_sched_num = 8;
+        co::init();
+    }
+
     unitest::run_all_tests();
-    co::exit();
+
+    if (!FLG_noco) co::exit();
     return 0;
 }
