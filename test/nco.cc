c34e332d65d68aaa5713c41bdc1eb83f7a95b9f4
idealvin <idealvin@qq.com>
2021-10-31 19:29:43 +0800

    update test/nco

--- test/nco.cc
+++ test/nco.cc
@@ -1,11 +1,12 @@
 #include "co/co.h"
 
 DEF_uint32(n, 1000000, "coroutine number");
+DEF_uint32(t, 7, "seconds to sleep in coroutines");
 
 DEF_main(argc, argv) {
     for (int i = 0; i < FLG_n; ++i) {
         go([](){
-            co::sleep(1000000);
+            co::sleep(FLG_t * 1000);
         });
     }
 
