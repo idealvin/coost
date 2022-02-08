98877576dcdc6a63bb4723c29d41ce6f453b6e20
idealvin <idealvin@qq.com>
2022-02-08 20:24:28 +0800

    add some unit tests

--- unitest/alien.cc
+++ unitest/alien.cc
@@ -1,11 +1,68 @@
 #include "co/unitest.h"
 #include <string.h>
 
+#ifdef _WIN32
+#include <intrin.h>
+#endif
+
 namespace test {
 
 void parse_serv_url(const char* url, fastring& ip, int& port, bool& https);
 
+inline uint32 find_index(uint32 size) {
+  #ifdef _WIN32
+    unsigned long index;
+    _BitScanReverse(&index, size - 1);
+    return index - 3;
+  #else
+    return 28 - __builtin_clz(size - 1);
+  #endif
+}
+
+inline uint32 find_first_zero_bit(uint32 x) {
+  #ifdef _WIN32
+    unsigned long index;
+    return _BitScanForward(&index, ~x) ? index : 32;
+  #else
+    const uint32 r = __builtin_ffs(~x);
+    return r > 0 ? r - 1 : 32;
+  #endif
+}
+
 DEF_test(alien) {
+    DEF_case(find_index) {
+        EXPECT_EQ(find_index(1u << 4), 0);
+        EXPECT_EQ(find_index(1u << 5), 1);
+        EXPECT_EQ(find_index(1u << 6), 2);
+        EXPECT_EQ(find_index(1u << 7), 3);
+        EXPECT_EQ(find_index(1u << 8), 4);
+        EXPECT_EQ(find_index(1u << 9), 5);
+        EXPECT_EQ(find_index(1u << 10), 6);
+        EXPECT_EQ(find_index(1u << 11), 7);
+        EXPECT_EQ(find_index(1u << 12), 8);
+
+      #if 0
+        for (uint32 x = 4; x < 12; ++x) {
+            for (uint32 i = (1 << x) + 1; i < (1 << (x + 1)); ++i) {
+                EXPECT_EQ(find_index(i), x - 3);
+            }
+        }
+      #endif
+    }
+
+    DEF_case(find_first_zero_bit) {
+        for (uint32 i = 1; i < 32; ++i) {
+            EXPECT_EQ(find_first_zero_bit((1u << i) - 1), i);
+            EXPECT_EQ(find_first_zero_bit((1u << i)), 0);
+        }
+
+        EXPECT_EQ(find_first_zero_bit(5), 1);
+        EXPECT_EQ(find_first_zero_bit(23), 3);
+        EXPECT_EQ(find_first_zero_bit(73), 1);
+        EXPECT_EQ(find_first_zero_bit(107), 2);
+        EXPECT_EQ(find_first_zero_bit((uint32)-1), 32);
+    }
+
     DEF_case(parse_serv_url) {
         fastring ip;
         int port;
