659048f235301d090bbc8a57c3a469342727caf8
idealvin <idealvin@qq.com>
2022-04-23 20:22:42 +0800

    update unitest for co/alloc

--- unitest/alloc.cc
+++ unitest/alloc.cc
@@ -5,44 +5,44 @@ namespace test {
 
 DEF_test(alloc) {
     DEF_case(static) {
-        void* p = co::alloc_static(24);
+        void* p = co::static_alloc(24);
         EXPECT_NE(p, (void*)0);
 
         *(uint32*)p = 7;
         EXPECT_EQ(*(uint32*)p, 7);
 
-        int* x = co::new_static<int>(7);
+        int* x = co::static_new<int>(7);
         EXPECT_NE(x, (void*)0);
         EXPECT_EQ(*x, 7);
     }
 
     DEF_case(fixed) {
-        void* p = co::alloc_fixed(8);
+        void* p = co::fixed_alloc(8);
         EXPECT_NE(p, (void*)0);
 
         *(uint32*)p = 7;
         EXPECT_EQ(*(uint32*)p, 7);
 
         void* x = p;
-        co::free_fixed(p, 8);
+        co::free(p, 8);
 
-        p = co::alloc_fixed(72);
+        p = co::fixed_alloc(72);
         EXPECT_NE(p, (void*)0);
         EXPECT_EQ(p, x);
-        co::free_fixed(p, 72);
+        co::free(p, 72);
 
-        p = co::alloc_fixed(2048);
+        p = co::fixed_alloc(2048);
         EXPECT_EQ(p, x);
-        co::free_fixed(p, 2048);
+        co::free(p, 2048);
 
-        p = co::alloc_fixed(4096);
+        p = co::fixed_alloc(4096);
         EXPECT_NE(p, (void*)0);
         EXPECT_NE(p, x);
-        co::free_fixed(p, 4096);
+        co::free(p, 4096);
 
-        int* v = co::new_fixed<int>(7);
+        int* v = co::make<int>(7);
         EXPECT_EQ(*v, 7);
-        co::delete_fixed(v);
+        co::del(v);
     }
 
     DEF_case(deflt) {
@@ -68,37 +68,22 @@ DEF_test(alloc) {
         EXPECT_EQ(*(uint32*)p, 7);
 
         x = p;
-        p = co::realloc(p, 4 * 1024, 8 * 1024);
+        p = co::realloc(p, 4096, 8 * 1024);
         EXPECT_EQ(p, x);
         EXPECT_EQ(*(uint32*)p, 7);
 
-        p = co::realloc(p, 8 * 1024, 32 * 1024);
+        p = co::realloc(p, 8 *1024, 32 * 1024);
         EXPECT_EQ(p, x);
 
         p = co::realloc(p, 32 * 1024, 64 * 1024);
         EXPECT_EQ(p, x);
 
-        p = co::realloc(p, 64 * 1024, 68 * 1024);
+        x = p;
+        p = co::realloc(p, 64 * 1024, 132 * 1024);
         EXPECT_NE(p, x);
         EXPECT_EQ(*(uint32*)p, 7);
 
-        x = p;
-        p = co::realloc(p, 68 * 1024, 128 * 1024);
-        EXPECT_EQ(p, x);
-        co::free(p, 128 * 1024);
-
-        p = co::alloc(96 * 1024);
-        EXPECT_NE(p, (void*)0);
-        *(uint32*)p = 7;
-        EXPECT_EQ(*(uint32*)p, 7);
-
-        x = p;
-        p = co::realloc(p, 96 * 1024, 128 * 1024);
-        EXPECT_EQ(p, x);
-        EXPECT_EQ(*(uint32*)p, 7);
-
-        p = co::realloc(p, 128 * 1024, 256 * 1024);
-        EXPECT_NE(p, (void*)0);
+        p = co::realloc(p, 132 * 1024, 256 * 1024);
         EXPECT_EQ(*(uint32*)p, 7);
         co::free(p, 256 * 1024);
     }
