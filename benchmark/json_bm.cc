cba0b96ae7a1453d11ea00d1fd65d32a3fdc9585
idealvin <idealvin@qq.com>
2022-06-11 20:25:14 +0800

    cleanup json_bm

--- benchmark/json_bm.cc
+++ benchmark/json_bm.cc
@@ -16,21 +16,6 @@ DEF_bool(minimal, false, "");
 int main(int argc, char** argv) {
     flag::init(argc, argv);
 
-    fs::file bob("bob.json", 'r');
-    fastring kk = bob.read(bob.size());
-    COUT << "bob size: " << bob.size();
-
-    Json bjs = json::parse(kk);
-    auto yy = bjs.pretty();
-    COUT << (yy == kk);
-
-    auto zz = bjs.str();
-    Json mjs = json::parse(zz);
-    auto vv = mjs.pretty();
-    COUT << (vv == kk);
-    COUT << vv;
-    return 0;
-
     fs::file f(FLG_s.c_str(), 'r');
     if (!f) {
         COUT << "open file failed: " << FLG_s;
@@ -50,16 +35,12 @@ int main(int argc, char** argv) {
     {
         {
             Json v = json::parse(s);
-            COUT << v.get("search_metadata", "count").as_int() << " results.";
-            COUT << v.get("statuses", 0, "metadata", "result_type").as_string();
-            COUT << v["statuses"][0]["in_reply_to_user_id_str"].as_string();
+            COUT << v["search_metadata"]["count"].as_int() << " results.";
+            COUT << v["statuses"][0]["metadata"]["result_type"].as_string();
             timer.restart();
             fastring s = v.str();
             COUT << "co/json str() time:" << timer.us();
             COUT << "str() size: " << s.size();
-            fs::file cox("cox.json", 'w');
-            cox.write(v.pretty());
-            cox.close();
         }
         timer.restart();
         for (int i = 0; i < FLG_n; ++i) {
@@ -74,14 +55,10 @@ int main(int argc, char** argv) {
             rapidjson::parse(s.c_str(), v);
             COUT << v["search_metadata"]["count"].GetInt() << " results.";
             COUT << v["statuses"][0]["metadata"]["result_type"].GetString();
-            COUT << v["statuses"][0]["in_reply_to_user_id_str"].GetString();
             timer.restart();
             std::string s = rapidjson::str(v);
             COUT << "rapidjson str() time:" << timer.us();
             COUT << "str() size: " << s.size();
-            fs::file xx("xx.json", 'w');
-            xx.write(rapidjson::pretty(v));
-            xx.close();
         }
         timer.restart();
         for (int i = 0; i < FLG_n; ++i) {
