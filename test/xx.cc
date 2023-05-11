7c1dad25230bb6fb2ff66a331f1b9b485a02ec0d
idealvin <idealvin@qq.com>
2023-05-11 20:40:39 +0800

    update test

--- test/xx.cc
+++ test/xx.cc
@@ -1,27 +1,7 @@
 #include "co/all.h"
 
-void cout_test() {
-    cout << text::red("hello\n");
-    cout << text::green("hello\n");
-    cout << text::blue("hello\n");
-    cout << text::yellow("hello\n");
-    cout << text::magenta("hello\n");
-    cout << text::cyan("hello\n");
-    cout << "hello\n";
-    cout << text::bold("hello\n");
-    cout << text::bold("hello\n").red();
-    cout << text::bold("hello\n").green();
-    cout << text::bold("hello\n").blue();
-    cout << text::bold("hello\n").yellow();
-    cout << text::bold("hello\n").magenta();
-    cout << text::bold("hello\n").cyan();
-    co::print("hello", text::red(" coost "), 23);
-    co::print("hello", text::bold(" coost ").green(), 23);
-    co::print("hello", text::blue(" coost "), 23);
-}
-
 int main(int argc, char** argv) {
     flag::parse(argc, argv);
-    cout_test();
+    co::print("hello coost");
     return 0;
 }
