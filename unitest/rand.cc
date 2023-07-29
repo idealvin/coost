743a22adeeca95468005c836b4486454a2360670
idealvin <idealvin@qq.com>
2023-07-29 21:35:51 +0800

    add unitest for rand

--- unitest/rand.cc
+++ unitest/rand.cc
@@ -12,11 +12,20 @@ DEF_test(rand) {
     EXPECT_GT(x, y);
     EXPECT_EQ((x - y + 100) * 10 / 10000, 2);
 
+    EXPECT(co::randstr(0).empty());
+    EXPECT(co::randstr(-1).empty());
+    EXPECT(co::randstr("abc", 0).empty());
+    EXPECT(co::randstr("abc", -1).empty());
+    EXPECT(co::randstr(NULL, 23).empty());
+    EXPECT(co::randstr("", 23).empty());
+
     fastring s = co::randstr();
     EXPECT_EQ(s.size(), 15);
 
     s = co::randstr(23);
     EXPECT_EQ(s.size(), 23);
+
+    EXPECT_EQ(co::randstr("x", 6), fastring(6, 'x'));
 
     s = co::randstr("ab", 8);
     EXPECT_EQ(s.size(), 8);
